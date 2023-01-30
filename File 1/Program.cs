e6b97367af21533f65267dd7146ceb03fb4422e6
Zverev 78 <9842905@gmail.com>
2023-01-30 11:01:01 +0300

    1

--- File 1/Program.cs
+++ File 1/Program.cs
@@ -8,12 +8,30 @@ namespace File_1
 {
     internal class Program
     {
-            // Задание 1.
-            // Воспользовавшись решением задания 3 четвертого модуля
-            // 1.1. Создать метод, принимающий число и матрицу, возвращающий матрицу умноженную на число
-            // 1.2. Создать метод, принимающий две матрицу, возвращающий их сумму
-            // 1.3. Создать метод, принимающий две матрицу, возвращающий их произведение
+        // Задание 1.
+        // Воспользовавшись решением задания 3 четвертого модуля
+        // 1.1. Создать метод, принимающий число и матрицу, возвращающий матрицу умноженную на число
+        // 1.2. Создать метод, принимающий две матрицу, возвращающий их сумму
+        // 1.3. Создать метод, принимающий две матрицу, возвращающий их произведение
 
+        // метод умножающий матрицу на число
+        static int matrixMultiFactor (int factor, int[,] arroy)
+        {
+           
+            foreach (int item in arroy)
+            {
+              Console.Write( item * factor);
+            }
+            //for (int i = 0; i < matrix.Length; i++)
+            //{
+            //    for (int j = 0; j < matrix.Length; j++)
+            //    {
+            //        matrix[i, j] = matrix[i, j] * factor;
+            //        //Console.Write($"{matrix[i, j]}\t");
+            //    }
+            //}
+            return arroy;
+        }
 
         // Метод умножающий две матрицы
         //static int multiMatrix (int [,] matrix, int [,] matrix2) 
@@ -21,22 +39,9 @@ namespace File_1
         //    return matrixRes;
         //}
 
-        // метод умножающий матрицу на число
-        //static int matrixMultiFactor (int[,]matrix,int factor)
-        //{
-        //    for (int i = 0; i < matrix.Length; i++)
-        //    {
-        //        for (int j = 0; j < matrix.Length; j++)
-        //        {
-        //            matrix[i, j] = matrix[i, j] * factor;
-        //            //Console.Write($"{matrix[i, j]}\t");
-        //        }
-        //    }
-        //    return matrix;
-        //}
 
         // метод производящий сложение двух матриц
- 
+
         static void Main(string[] args)
         {
             Console.Write("введите количество строк матрицы:");
@@ -67,16 +72,36 @@ namespace File_1
 
             Console.WriteLine("Исходная матрица два:");
             // заполнение второй матрицы
+            int count = 0;
             foreach (int item in matrix2)
             {
+
                 Console.Write($"{random.Next(1, 101)}\t");
+                count++;
+                if (count % col == 0)
+                {
+                    Console.WriteLine();
+                    count = 0;
+                }
             }
+            Console.WriteLine();
 
-            Console.WriteLine("Итоговая матрица: ");
+            Console.WriteLine("Итоговая матрица от умножения на число: ");
 
-            //matrix = matrixMultiFactor (matrix, factor);
-            // Console.WriteLine(matrix);
+            matrix = matrixMultiFactor(factor, matrix);
+            //int count = 0;
+            //foreach (int item in matrix)
+            //{
 
+            //    Console.Write($"{matrix}\t");
+            //    count++;
+            //    if (count % col == 0)
+            //    {
+            //        Console.WriteLine();
+            //        count = 0;
+            //    }
+            //}
+            //Console.WriteLine();
 
             //
             Console.ReadKey();
