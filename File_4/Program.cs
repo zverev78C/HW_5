915e74980b55944e561a973cd22333f823949c44
Zverev 78 <9842905@gmail.com>
2023-02-02 16:57:37 +0300

    sr5tu

--- File_4/Program.cs
+++ File_4/Program.cs
@@ -8,8 +8,6 @@ namespace File_4
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
             // Задание 4. Написать метод принимающий некоторое количесво чисел, выяснить
             // является заданная последовательность элементами арифметической или геометрической прогрессии
             // 
@@ -17,6 +15,12 @@ namespace File_4
             //             http://ru.wikipedia.org/wiki/Арифметическая_прогрессия
             //             http://ru.wikipedia.org/wiki/Геометрическая_прогрессия
             //
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Это прогрмамма для сравнения ряда чисел на наличие Арифметической или Геометрической прогрессии.");
+            int Vol = 1;
+            int[] Numders = new int[Vol]
+            Console.ReadLine();
         }
     }
 }
