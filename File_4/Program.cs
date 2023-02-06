46c1e1cad9febc77e027351b113ca7210a7a5ccb
Zverev 78 <9842905@gmail.com>
2023-02-06 15:23:37 +0300

    4

--- File_4/Program.cs
+++ File_4/Program.cs
@@ -8,26 +8,52 @@ namespace File_4
 {
     internal class Program
     {
-            // Задание 4. Написать метод принимающий некоторое количесво чисел, выяснить
-            // является заданная последовательность элементами арифметической или геометрической прогрессии
-            // 
-            // Примечание
-            //             http://ru.wikipedia.org/wiki/Арифметическая_прогрессия
-            //             http://ru.wikipedia.org/wiki/Геометрическая_прогрессия
-            //
+        // Задание 4. Написать метод принимающий некоторое количесво чисел, выяснить
+        // является заданная последовательность элементами арифметической или геометрической прогрессии
+        // 
+        // Примечание
+        //             http://ru.wikipedia.org/wiki/Арифметическая_прогрессия
+        //             http://ru.wikipedia.org/wiki/Геометрическая_прогрессия
+        //
         static void Main(string[] args)
-        {
+            {
             Console.WriteLine("Это прогрмамма для сравнения ряда чисел на наличие Арифметической или " +
             "Геометрической прогрессии.");
-            int [] Numbers;
+            int[] Numbers;
             Numbers = ReadNumb();
+            bool NewCheck = true;
+            int Arith = Numbers[1] - Numbers[0];
+            for (int i = 1; i < Numbers.Length; i++)
+            {
+             int TempValue = Numbers[i] - Numbers[i-1];
+             if (Arith != TempValue)
+             {
+              Console.WriteLine("Последовотельность не является арифметической");
+              NewCheck = false;
+              break;
+             } 
+            }
+
+            if (NewCheck == false)
+                {
+                float Geom = Numbers[1] % Numbers[0];
+                for (int i = 1; i < Numbers.Length; i++)
+                    {
+                    float TempValue = Numbers[i] % Numbers[i - 1];
+                    if (Geom != TempValue)
+                        {
+                        Console.WriteLine("Последовотельность не является Геометрической");
+                        break;
+                        }
+                    }
+                Console.WriteLine("Последовотельность является Геометрической");
+                }
+            else { Console.WriteLine("Последовотельность является арифметической");}
+
             foreach (int i in Numbers)
                 {
                 Console.Write($"{i} ");
                 }
-
-
-
             Console.ReadLine();
         }
         static void Check(int[]Numbers)
@@ -50,13 +76,12 @@ namespace File_4
         {
             Console.WriteLine("Сколько чисел вы хотите проверить?");
             int vol = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите числа");
+            Console.WriteLine("Введите числа  ");
             int[] arroy = new int[vol];
             for(int i =0; i<arroy.Length; i++)
             {
                 arroy[i] = Convert.ToInt32(Console.ReadLine());
-            }
-             
+            }                                                      
           return arroy;
         }    // метод принимающий числа
     }
