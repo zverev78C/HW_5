2a5e8b2f043b9e87ea809b5401d9e13422faf7b9
Zverev 78 <9842905@gmail.com>
2023-02-10 14:36:23 +0300

    5

--- File_5/Program.cs
+++ File_5/Program.cs
@@ -15,9 +15,23 @@ namespace File_5
             // 
     internal class Program
     {
+            static int A(int n, int m)
+                {
+                if (n == 0) return m + 1;
+                if (n != 0 && m == 0) return A(n - 1, 1);
+                if (n > 0 && m > 0) return A(n - 1, A(n, m - 1));
+                return A(n, m);
+                }
+
         static void Main(string[] args)
         {
-            int[] aker = new int[2];
-        }
+            Console.Write("Введите число n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число m: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine( A(n, m));
+
+            Console.ReadKey();
+            }
     }
 }
